cd529f0116e2608a18a4be4e7cb76547064eae52
Frederico Thomaz <fredericoufsj@yahoo.com.br>
2022-03-10 22:37:08 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,32 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import './styles.css';
 
-function Listing(){
-    return(
-        <Pagination></Pagination>
+function Listing() {
+    return (
+        <>
+            <Pagination></Pagination>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard></MovieCard>
+                    </div>
+                </div>
+            </div>
+        </>
+
     );
 }
 export default Listing;
